60d88d6179b1bf104342e7e8f71ad56c4efc6eb2
mohitchauhan4u <mohitchauhan1929@gmail.com>
2021-11-25 08:37:10 +0530

    added openActivity function it to onClick on button left with camera opening intent only in When_You_Are_Out_Of_Home_7.java

--- app/src/main/java/com/mohitchauhan4u/covidprotector/When_You_Are_Out_Of_Home_7.java
+++ app/src/main/java/com/mohitchauhan4u/covidprotector/When_You_Are_Out_Of_Home_7.java
@@ -2,7 +2,9 @@ package com.mohitchauhan4u.covidprotector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class When_You_Are_Out_Of_Home_7 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class When_You_Are_Out_Of_Home_7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_when_you_are_out_of_home7);
+    }
+    public void openActivity(View v){
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
