d4b9ecca4dea7b72e82db89a9a1e8a884ef85037
mohitchauhan4u <mohitchauhan1929@gmail.com>
2021-11-26 20:44:53 +0530

    created About_Covid and About_Covid.java file

---
+++ app/src/main/java/com/mohitchauhan4u/covidprotector/About_Covid.java
@@ -0,0 +1,14 @@
+package com.mohitchauhan4u.covidprotector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class About_Covid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_covid);
+    }
+}
