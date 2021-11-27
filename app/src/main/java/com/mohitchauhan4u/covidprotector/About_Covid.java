1239f7c9bb4097f54ed16aa3244ff344f7d2eb4a
mohitchauhan4u <mohitchauhan1929@gmail.com>
2021-11-27 14:18:00 +0530

    all changes before adding youtube video api

--- app/src/main/java/com/mohitchauhan4u/covidprotector/About_Covid.java
+++ app/src/main/java/com/mohitchauhan4u/covidprotector/About_Covid.java
@@ -3,12 +3,17 @@ package com.mohitchauhan4u.covidprotector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class About_Covid extends AppCompatActivity {
+    ListView listView;
+    String[] arr={"What is COVID-19?","How does the virus spreads?",};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_covid);
+        CustomAdapter ad = new CustomAdapter(this, R.layout.custom_laytout, arr);
+        listView.setAdapter(ad);
     }
 }
--- app/src/main/java/com/mohitchauhan4u/covidprotector/MainActivity.java
+++ app/src/main/java/com/mohitchauhan4u/covidprotector/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity{
     ListView listView;
-    String[] arr={"Prevent the spread of COVID-19","Check if you have Covid-19 Symptoms","Precautions to take when you are out of home","About Covid-19"};
+    String[] arr={"Prevent the spread of COVID-19","Check if you have COVID-19 Symptoms","Precautions to take when you are out of home","About COVID-19"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,8 +38,8 @@ public class MainActivity extends AppCompatActivity{
                     Intent intent=new Intent(MainActivity.this,When_You_Are_Out_Of_Home_1.class);
                     startActivity(intent);
                 }else if(position==3){
-//                    Intent intent=new Intent(MainActivity.this,prevent_spread_1.class);
-//                    startActivity(intent);
+                    Intent intent=new Intent(MainActivity.this,About_Covid.class);
+                    startActivity(intent);
                 }
             }
         });
