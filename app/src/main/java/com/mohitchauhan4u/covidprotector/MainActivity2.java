1981106b1127b2d7c6af16e7821443db68daf9ac
mohitchauhan4u <mohitchauhan1929@gmail.com>
2021-11-28 14:47:46 +0530

    project completed

--- app/src/main/java/com/mohitchauhan4u/covidprotector/MainActivity2.java
+++ app/src/main/java/com/mohitchauhan4u/covidprotector/MainActivity2.java
@@ -10,7 +10,7 @@ import android.widget.ListView;
 
 public class MainActivity2 extends AppCompatActivity {
     ListView listView;
-    String[] arr={"Prevent the spread of COVID-19","Check if you have COVID-19 symptoms","Precautions to take when you are out of home","About COVID-19"};
+    String[] arr={"Prevent the spread of COVID-19","Check if you have COVID-19 symptoms","Precautions to take when you are out of home","FAQ's About COVID-19"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +33,8 @@ public class MainActivity2 extends AppCompatActivity {
                     Intent intent=new Intent(MainActivity2.this,When_You_Are_Out_Of_Home_1.class);
                     startActivity(intent);
                 }else if(position==3){
-//                    Intent intent=new Intent(MainActivity.this,About_Covid.class);
-//                    startActivity(intent);
+                    Intent intent=new Intent(MainActivity2.this,AboutCovid.class);
+                    startActivity(intent);
                 }
             }
         });
